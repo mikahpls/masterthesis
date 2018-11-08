82a5e24b7d03b7a82040795bad312431eee920d0
Michael <michael@felleisen.de>
2018-11-08 15:12:37 +0100

    UITopMenuText working, UIBottomButtons working, Time working

---
+++ Masterthesis/Assets/Scripts/UI/UIMainPanelSwitcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMainPanelSwitcher : MonoBehaviour
+{
+    public Text UITopMenuText;
+
+    public void SwitchMainPanelTo(string name)
+    {
+        var list = transform.GetComponentsInChildren<Transform>(true).ToList().Where(t => t.tag == "MainPanel");
+        foreach (var t in list)
+        {
+            t.gameObject.SetActive(false);
+        }
+
+        list = transform.GetComponentsInChildren<Transform>(true).ToList().Where(t => t.name == name);
+        if (list.Count() == 0 && list.Count() >= 2)
+        {
+            Debug.Log("No or multiple MainPanel-tagged Objects with name: " + name + " found.");
+            return;
+        }
+
+        list.First().gameObject.SetActive(true);
+        UITopMenuText.text = name;
+    }
+
+}
---
+++ Masterthesis/Assets/Scripts/UI/UITimeUpdate.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITimeUpdate : MonoBehaviour
+{
+    public Text UITopMenuTimeText;
+
+    private float _cooldown = 70;
+
+	void Update ()
+	{
+	    _cooldown += Time.deltaTime;
+	    if (_cooldown > 60)
+	    {
+	        _cooldown = 0;
+	        UITopMenuTimeText.text = GetFormatedTimeString();
+	    }
+	}
+
+    private string GetFormatedTimeString()
+    {
+        var timenow = System.DateTime.Now;
+        string h = "";
+        var hours = timenow.Hour;
+        if (hours < 10)
+        {
+            h = "0" + hours;
+        }
+        else
+        {
+            h = hours.ToString();
+        }
+        string m = "";
+        var minutes = timenow.Minute;
+        if (minutes < 10)
+        {
+            m = "0" + minutes;
+        }
+        else
+        {
+            m = minutes.ToString();
+        }
+        return h + ":" + m;
+    }
+}
