973c5114392fda9a53bf02d512d7c9cc92279337
Michael <michael@felleisen.de>
2018-11-15 22:27:57 +0100

    added more gods to information json. now contains image names and url links as well.
    images are getting loaded in runtime from resource folder. it is now possible to open wiki pages from app

--- Masterthesis/Assets/Scripts/UI/UIInformationLoader.cs
+++ Masterthesis/Assets/Scripts/UI/UIInformationLoader.cs
@@ -8,9 +8,11 @@ public class UIInformationLoader : MonoBehaviour
 {
     public Text Name, Information;
     public Image Image;
+    public string currentURL;
 
     private static InformationText[] _informationTextArray;
 
+    //Puts information from json file to the information screen and looks up the matching image + loads it as sprite
     public void LoadInformation(string name)
     {
         TextAsset file = Resources.Load("information") as TextAsset;
@@ -23,10 +25,29 @@ public class UIInformationLoader : MonoBehaviour
             {
                 Name.text = i.Name;
                 Information.text = i.Information;
+                Image.sprite = LoadImageFromResources("Images/Gods/" + i.ImageName);
+                currentURL = i.URL;
                 return;
             }
         }
-        Debug.Log("No god with name: " + name + " found in information.txt");
+        Debug.Log("No god with name: " + name + " found in Resources/information.txt");
+    }
+
+    private Sprite LoadImageFromResources(string path)
+    {
+        var sprite = Resources.Load<Sprite>(path);
+
+        if (sprite == null)
+        {
+            Debug.Log("No Image found in Resources at path: " + path + " found.");
+        }
+
+        return sprite;
+    }
+
+    public void OpenBrowser()
+    {
+        Application.OpenURL(currentURL);
     }
 }
 
@@ -35,4 +56,6 @@ public class InformationText
 {
     public string Name;
     public string Information;
+    public string ImageName;
+    public string URL;
 }
