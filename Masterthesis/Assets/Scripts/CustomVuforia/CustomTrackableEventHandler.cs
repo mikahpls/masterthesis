6545c293ed56e7177646e3b87e1a997e32e8fe25
Michael <michael@felleisen.de>
2019-01-24 15:07:51 +0100

    had to import complete nike as "animated" armature because previous project did it that way and only that way..

---
+++ Masterthesis/Assets/Scripts/CustomVuforia/CustomTrackableEventHandler.cs
@@ -0,0 +1,105 @@
+﻿//edited version of DefaultTrackableEventHandler
+
+using UnityEngine;
+using Vuforia;
+
+public class CustomTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+    //CUSTOM: name of model to activate matching ui panels
+    public string Name;
+
+    protected TrackableBehaviour mTrackableBehaviour;
+    protected TrackableBehaviour.Status m_PreviousStatus;
+    protected TrackableBehaviour.Status m_NewStatus;
+
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+
+        //CUSTOM: register self on start to modeltargetcollection, might not be needed
+        GetComponentInParent<ModelTargetCollection>().RegisterModelTarget(transform);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        m_PreviousStatus = previousStatus;
+        m_NewStatus = newStatus;
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+            OnTrackingFound();
+
+            //CUSTOM: tell modeltargetcollection that modeltarget was found and transfer name
+            GetComponentInParent<ModelTargetCollection>().ModelTargetFound(Name);
+
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+            OnTrackingLost();
+
+            //CUSTOM: tell modeltargetcollection that modeltarget was lost and transfer name
+            GetComponentInParent<ModelTargetCollection>().ModelTargetLost(Name);
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+    }
+
+    protected virtual void OnTrackingFound()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Enable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = true;
+
+        // Enable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = true;
+
+        // Enable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = true;
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Disable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = false;
+
+        // Disable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = false;
+
+        // Disable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = false;
+    }
+}
---
+++ Masterthesis/Assets/Scripts/CustomVuforia/ModelTargetCollection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelTargetCollection : MonoBehaviour
+{
+    public UIMainPanelSwitcher UiMainPanelSwitcher;
+
+    private List<Transform> _modelTargetList = new List<Transform>();
+
+    void Start()
+    {
+        if (UiMainPanelSwitcher == null) Debug.Log("UIMainPanelSwitcher in ModelTargetCollection is null.");
+    }
+
+    public void RegisterModelTarget(Transform t)
+    {
+        _modelTargetList.Add(t);
+    }
+
+    public void ModelTargetFound(string name)
+    {
+        
+    }
+
+    public void ModelTargetLost(string name)
+    {
+        
+    }
+}
