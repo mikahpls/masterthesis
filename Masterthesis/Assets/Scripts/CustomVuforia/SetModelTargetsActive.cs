f7b386c41d6b1f16bf3c30bf2637dd89b5798672
Michael <michael@felleisen.de>
2019-04-30 14:21:54 +0200

    dynamic model target loading now working without switching scene

--- Masterthesis/Assets/Scripts/CustomVuforia/SetModelTargetsActive.cs
+++ Masterthesis/Assets/Scripts/CustomVuforia/SetModelTargetsActive.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Vuforia;
 
 public class SetModelTargetsActive : MonoBehaviour {
 
     public List<GameObject> ModelTargetList = new List<GameObject>();
+    private ObjectTracker objTracker;
+    private DataSet currentDataSet;
+    private string nextTargetDSName;
 
 	//laokoon 0
     //aphaia 1
@@ -14,14 +19,33 @@ public class SetModelTargetsActive : MonoBehaviour {
         {
             obj.SetActive(false);
         }
-
         if(i == 0)
         {
             ModelTargetList[0].SetActive(true);
+            //use database name you gave the database in the target model generator of vuforia, can be found on ARCamera
+            nextTargetDSName = "laocoon_target";
         }
         else if(i == 1)
         {
             ModelTargetList[1].SetActive(true);
+            nextTargetDSName = "aphaia";
         }
+
+        //switch target model
+        LoadNewTargetModel(nextTargetDSName);
+    }
+
+    private void LoadNewTargetModel(string dataBaseName)
+    {
+        TrackerManager tm = (TrackerManager)TrackerManager.Instance;
+        objTracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+        if (objTracker == null) return;
+        currentDataSet = objTracker.GetActiveDataSets().FirstOrDefault();
+        objTracker.Stop();
+        objTracker.DeactivateDataSet(currentDataSet);
+        currentDataSet = objTracker.CreateDataSet();
+        currentDataSet.Load(dataBaseName);
+        objTracker.ActivateDataSet(currentDataSet);
+        objTracker.Start();
     }
 }
